7111319b48a8ae6a433daf16567d5db78f08d457
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-09-22 01:55:51 +0900

    Implement mylist

--- lib/niconico/mylist.rb
+++ lib/niconico/mylist.rb
@@ -2,8 +2,11 @@
 
 require 'niconico/mylist/config'
 require 'niconico/mylist/client'
+require 'niconico/mylist/item'
 
 require 'niconico/mylist/version'
+
+require 'time'
 
 class Niconico::Mylist
   ENDPOINT = 'http://www.nicovideo.jp'
@@ -34,5 +37,43 @@ class Niconico::Mylist
 
   def initialize(data)
     @data = data
+  end
+
+  def title
+    xpath_text('title')
+  end
+
+  def link
+    xpath_text('link')
+  end
+
+  def description
+    xpath_text('description')
+  end
+
+  def pub_date
+    Time.parse(xpath_text('description'))
+  end
+
+  def last_build_date
+    Time.parse(xpath_text('description'))
+  end
+
+  def creator
+    xpath_text('dc:creator')
+  end
+
+  def items
+    xpath_match('item').map {|item| Niconico::Mylist::Item.new(item) }
+  end
+
+  private
+
+  def xpath_text(path)
+    REXML::XPath.first(@data, "channel/#{path}").text
+  end
+
+  def xpath_match(path)
+    REXML::XPath.match(@data, "channel/#{path}")
   end
 end
